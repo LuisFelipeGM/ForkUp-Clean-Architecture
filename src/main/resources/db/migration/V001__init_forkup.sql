24c71262b1f9c4c366a90db6670adfe2e234807c
LuisFelipeGM <lipe.garcia.menezes@gmail.com>
2026-07-08 00:03:16 -0300

    feat: adicionando insert inicial na tabela tipo usuario

--- src/main/resources/db/migration/V001__init_forkup.sql
+++ src/main/resources/db/migration/V001__init_forkup.sql
@@ -45,3 +45,8 @@ CREATE TABLE IF NOT EXISTS forkup.item_cardapio (
     foto_url VARCHAR(255),
     restaurante_id UUID NOT NULL REFERENCES forkup.restaurante(id)
 );
+
+INSERT INTO forkup.tipo_usuario (id, descricao) VALUES
+    ('2f8c9a0b-2d3e-4b6c-9a0b-2d3e4b6c9a0b', 'Dono de Restaurante'),
+    ('1e7b8f9e-1c2d-4a5b-8f9e-1c2d4a5b8f9e', 'Cliente'),
+    ('3f9d0b1c-3e4f-5c7d-8b1c-3e4f5c7d0b1c', 'Administrador');
